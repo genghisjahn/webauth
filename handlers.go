b3bee3f149161057108e5bfe5f953296226bfdcc
Jon Wear <jonwear@fastmail.fm>
2014-11-21 20:04:41 -0500

    remove logging

--- handlers.go
+++ handlers.go
@@ -3,5 +3,6 @@ package main
 import "net/http"
 
 func authHandler(w http.ResponseWriter, r *http.Request) {
-	http.Error(w, "Not Implemented.", http.StatusInternalServerError)
+	//http.Error(w, "Not Implemented.", http.StatusInternalServerError)
+	return nil
 }
